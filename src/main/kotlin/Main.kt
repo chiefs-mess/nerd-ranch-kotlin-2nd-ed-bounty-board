b6f3ef502ae540eb2934f462b5fcdd7f25cdae2b
The Wanderer <kv1qer20@duck.com>
2025-01-18 21:58:29 +0000

    Chapter 2 - End

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+const val HERE_NAME = "Madrigal"
+
+fun main() {
+    println("The hero announces her presence to the world.")
+
+    println(HERE_NAME)
+    var playerLevel = 4
+    println(playerLevel)
+
+    println("The hero embarks on her journey to locate the enchanted sword.")
+    playerLevel += 1
+    println(playerLevel)
+}
