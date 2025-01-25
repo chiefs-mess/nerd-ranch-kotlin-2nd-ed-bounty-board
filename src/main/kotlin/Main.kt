e525615c278816eadedcab2c8d121d6a529230b1
The Wanderer <kv1qer20@duck.com>
2025-01-25 22:24:11 +0000

    Chapter 3 - End

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,37 @@
-const val HERE_NAME = "Madrigal"
+const val HERO_NAME = "Madrigal"
 
 fun main() {
     println("The hero announces her presence to the world.")
-
-    println(HERE_NAME)
+    println(HERO_NAME)
     var playerLevel = 4
     println(playerLevel)
+    val hasBefriendedBarbarians = true
+    val hasAngeredBarbarians = false
+    val playerClass = "paladin"
+    val quest: String = when (playerLevel) {
+        1 -> "Meet Mr. Bubbles in the land of soft things."
+        in 2..5 -> {
+            // Check whether diplomacy is an option
+            val canTalkToBarbarians = !hasAngeredBarbarians &&
+                    (hasBefriendedBarbarians || playerClass == "barbarian")
 
-    println("The hero embarks on her journey to locate the enchanted sword.")
+            if (canTalkToBarbarians) {
+                "Convince the barbarians to call off their invasion."
+            } else {
+                "Save the town from the barbarian invasions."
+            }
+        }
+        6 -> "Locate the enchanted sword."
+        7 -> "Recover the long-lost artifact of creation."
+        8 -> "Defeat Nogartse, bringer of death and eater of worlds."
+        else -> "There are no quests right now."
+    }
+
+    println("The hero approaches the bounty board. It reads:")
+    println(quest)
+
+    println("Time passes...")
+    println("The hero returns from her quest.")
     playerLevel += 1
     println(playerLevel)
 }
